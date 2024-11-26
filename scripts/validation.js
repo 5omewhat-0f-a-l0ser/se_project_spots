ca5d08000a215774b9c734998bc0752de935bbf6
Millie Houston <Immaloser102@gmail.com>
2024-11-26 14:34:22 -0800

    I'm lost, but ok?

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,9 +1,24 @@
+const setEventListeners = (formEL) => {
+  const inputList = Array.from(formEL.querySelectorAll(".modal__input"));
+  const buttonElement = formEL.querySelector(".modal__submit");
 
+  console.log(inputList);
+  console.log(buttonElement);
 
+  //toggleButtonState(inputList, buttonElement);
 
-
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEL, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
 
 const enableValidation = () => {
-    
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEL) => {
+    setEventListeners(formEL);
+  });
   };
-  enableValidati();
+  enableValidation();
