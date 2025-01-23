bb63cc9967cb36ed36c335a870297e13ae43aa6b
Millie Houston <Immaloser102@gmail.com>
2025-01-22 20:19:41 -0800

    And I'm still typing.... Still typing... Hey, there's an echo!

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -6,7 +6,17 @@ class Api {
     }
   
     getInitialCards() {
-      
+        return fetch("https://around-api.en.tripleten-services.com/v1", {
+          headers: {
+            authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6"
+          }
+        })
+          .then(res => {
+            if (res.ok) {
+              return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+          });
     }
   
     // other methods for working with the API
