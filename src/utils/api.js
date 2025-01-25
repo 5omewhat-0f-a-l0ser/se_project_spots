fa0677698ad1428889173f663a5a55ece3fd1c62
Millie Houston <Immaloser102@gmail.com>
2025-01-24 22:08:06 -0800

    Adding cardsgit add -A

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 class Api {
     constructor( {baseUrl, headers} ) {
       this._baseUrl = baseUrl;
-      this._headers = headers;
+      this._headers = headers; 
     }
 
     getAppInfo() {
-      return Promise.all([this.getInitialCards(), this.getUserInfo(), this.editUserInfo()])
+      return Promise.all([this.getInitialCards(), this.getUserInfo()])
     }
 
   
@@ -25,7 +25,8 @@ class Api {
     }
   
     // create more methods(ie: getUserInfo, etc.) for the api with a diff baseUrl
-
+    //--user/avatar functions--
+  
     getUserInfo() {
       return fetch(`${this._baseUrl}/users/me`, {
         method: 'GET',
@@ -35,15 +36,10 @@ class Api {
         
     }
 
-
-
     editUserInfo({ name, about }) {
       return fetch(`${this._baseUrl}/users/me`, {
         method: 'PATCH',
-        headers: {
-          authorization: this._headers,
-          'Content-Type': 'application/json'
-        },
+        headers: this._headers,
         body: JSON.stringify({
           name,
           about,
@@ -52,6 +48,28 @@ class Api {
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
+    }
+
+    //--card functions--
+
+    addNewCard() {
+      return fetch(`${this._baseUrl}/cards`, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: cardName, // cardName should be your card's name variable
+          link: cardLink  // cardLink should be your card's link variable
+        })
+      })
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch(error => {
+          console.log('Error fetching data', error);
+        });
     }
 }
   
